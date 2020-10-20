df15ffe1f7ecc89979b0f12b4488b2337b1bd223
Шляпкин Иван Николаевич <Shlyapkin.I.Ni@sberbank.ru>
2020-10-20 03:44:38 +0300

    Small fixes

--- tredernet/MainInteractor.swift
+++ tredernet/MainInteractor.swift
@@ -9,23 +9,26 @@ import SocketIO
 
 final class MainInteractor {
 	weak var presenter: MainInteractorOutput?
-	private let manager = SocketManager(socketURL: URL(string: "https://ws.tradernet.ru")!, config: [.log(true)])
+	private var manager: SocketManager?
 	private var tickerSet = Set<String>()
 }
 
 extension MainInteractor: MainInteractorInput {
 	func configureConnection() {
-		let socket = manager.defaultSocket
+		guard let url = URL(string: "https://ws.tradernet.ru") else { return }
+		manager = SocketManager(socketURL: url, config: [.log(true)])
 
-		socket.on(clientEvent: .connect) { data, ack in
+		let socket = manager?.defaultSocket
+
+		socket?.on(clientEvent: .connect) { data, ack in
 			let tickersToWatchChanges = [ "RSTI", "GAZP", "MRKZ","RUAL","HYDR","MRKS","SBER","FEES","TGKA",
 										  "VTBR","ANH.US","VICL.US","BURG.US","NBL.US","YETI.US","WSFS.US",
 										  "NIO.US","DXC.US","MIC.US","HSBC.US","EXPN.EU","GSK.EU","SHP.EU",
 										  "MAN.EU","DB1.EU","MUV2.EU","TATE.EU","KGF.EU","MGGT.EU","SGGD.EU" ]
-			socket.emit("sup_updateSecurities2", tickersToWatchChanges);
+			socket?.emit("sup_updateSecurities2", tickersToWatchChanges);
 		}
 
-		socket.on("q") { [weak self] data, ack in
+		socket?.on("q") { [weak self] data, ack in
 			guard let data = data.first, let jsonData = try? JSONSerialization.data(withJSONObject:data),
 				  let parsedResult: Response = try? JSONDecoder().decode(Response.self, from: jsonData) else { return }
 
@@ -40,6 +43,6 @@ extension MainInteractor: MainInteractorInput {
 			}
 		}
 
-		socket.connect()
+		socket?.connect()
 	}
 }
